9d523ee43c4e9479e23c922d71de00c76414f248
Kari Helene Bekkelund <bappleubuntu@bappelUbuntu.(none)>
2014-11-20 22:45:34 +0100

    deleted last files

--- lowlevel/examples/common_platform.h
+++
@@ -1,7 +0,0 @@
-#include <pthread.h>
-#include <assert.h>
-
-#define FMOD_Main() main(int, char**)
-#define Common_TTY(format, __VAR_ARGS__) fprintf(stderr, format, __VAR_ARGS__)
-
-typedef pthread_mutex_t Common_Mutex;
