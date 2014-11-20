f19e7b40b507f64e7e64927318ead65c91c29647
Kari Helene Bekkelund <bappleubuntu@bappelUbuntu.(none)>
2014-11-20 11:22:00 +0100

    deleted even more

--- test2.c
+++
@@ -1,9 +0,0 @@
-#include <ncurses.h>
-#include <unistd.h>
-int main(int argc, char *argv[]) {
-  initscr();
-  noecho();
-  curs_set(FALSE);
-  sleep(1);
-  endwin(); // Restore normal terminal behavior
-}
